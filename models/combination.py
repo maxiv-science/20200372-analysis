d57bdfe6188344749b434d8cdcee96cff1c6c90e
Alexander Bjorling <alexander.bjorling@maxiv.lu.se>
2023-01-16 13:17:44 +0100

    figure 5 now ready for the paper, with an SI figure about energies

--- models/combination.py
+++ models/combination.py
@@ -9,11 +9,12 @@ import numpy as np
 from scipy.integrate import odeint
 from scipy.interpolate import UnivariateSpline
 import matplotlib.pyplot as plt
-plt.rc('font', size=14)
+plt.rc('font', size=8)
 plt.ion()
+plt.close('all')
 
 
-def dcdt(C, t, D, r, r0, flux_density=0):
+def dcdt(C, t, D, r, r0, flux_density=0, mu=8.4e2):
 
     # odeint does 1d vectors, so our species are stacked - work on a view
     n_species = len(D)
@@ -26,7 +27,7 @@ def dcdt(C, t, D, r, r0, flux_density=0):
 
     # spur rates are (n_species,)
     dC[:, :] += (
-        spur_rates(C_rs, t, flux_density).reshape((-1, 1)) * H(r0 - r)
+        spur_rates(C_rs, t, flux_density, mu).reshape((-1, 1)) * H(r0 - r)
     )
 
     # homogeneous reactions
@@ -34,43 +35,75 @@ def dcdt(C, t, D, r, r0, flux_density=0):
 
     return dC.flatten()
 
-if True:
+if 0:
+    mus = [7.3e5, 8.4e2, 45]
     r0 = 50e-9
     r = np.logspace(-9, -3, 500)
-    t = np.logspace(-6, 1, 200)
-    y0 = np.zeros(7 * len(r))
-    y0.reshape((7, -1))[5, :] = 250e-6  # air-saturated
-    D = np.ones(7) * 5e-9
+    t = np.logspace(-6, 0, 200)
+    y0 = np.zeros(4 * len(r))
+    # [OH, H, H2, H2O2]
+    D = np.array((2.3, 5, 5, 2)) * 1e-9  # m2/s
     flux_density = 7e10 / (3.14 * r0**2)
 
-    print('with O2...')
-    c = odeint(dcdt, y0, t, args=(D, r, r0, flux_density))
-    c = c.reshape((len(t), len(D), -1))
-    print('without O2...')
-    c_ = odeint(dcdt, np.zeros_like(y0), t, args=(D, r, r0, flux_density))
-    c_ = c_.reshape((len(t), len(D), -1))
+    av_concs = []
+    for j, mu in enumerate(mus):
+        print(int(round(mu)))
+        c = odeint(dcdt, np.zeros_like(y0), t, args=(D, r, r0, flux_density, mu))
+        c = c.reshape((len(t), len(D), -1))
 
-    edge = np.where(r <= r0)[-1][-1]
-    mol_per_length = np.sum(c[:, :, :edge] * 2 * np.pi * r[:edge] * np.diff(r)[:edge].reshape((1, -1)), axis=-1)
-    mol_per_length_ = np.sum(c_[:, :, :edge] * 2 * np.pi * r[:edge] * np.diff(r)[:edge].reshape((1, -1)), axis=-1)
-
-    av_conc = mol_per_length / (np.pi * r0**2)
-    av_conc_ = mol_per_length_ / (np.pi * r0**2)
-    np.savez('combination.npz', av_conc=av_conc, av_conc_=av_conc_, t=t)
+        edge = np.where(r <= r0)[-1][-1]
+        mol_per_length = np.sum(c[:, :, :edge] * 2 * np.pi * r[:edge] * np.diff(r)[:edge].reshape((1, -1)), axis=-1)
+        av_concs.append(mol_per_length / (np.pi * r0**2))
+    av_concs=np.array(av_concs)
+    np.savez('combination.npz',
+             av_concs=av_concs, t=t, mus=np.array(mus))
 else:
     dct = np.load('combination.npz')
-    av_conc = dct['av_conc']
-    av_conc_ = dct['av_conc_']
+    av_concs = dct['av_concs']
+    mus = dct['mus']
     t = dct['t']
 
-plt.figure()
-plt.subplots_adjust(bottom=.2, left=.15, right=.99, top=.99)
-cols = 'kcrmgb'
-for i, lbl in enumerate(['OH', 'H', 'H2', 'H2O2']):
-    plt.plot(t, av_conc[:, i] * 1e3, color=cols[i], label=lbl)
-    plt.plot(t, av_conc_[:, i] * 1e3, ':', color=cols[i])
+# 8.5 keV for the paper
+av_conc = av_concs[1]
+fig = plt.figure(figsize=(3.33,2.5))
+fig.subplots_adjust(bottom=.17, left=.18, right=.99, top=.98)
+lines, labels = [], []
+for j, lbl in enumerate(['OH$^\\bullet$', 'H$^\\bullet$', 'H$_\mathrm{2}$', 'H$_\mathrm{2}$O$_\mathrm{2}$']):
+    lines.append(plt.plot(t, av_conc[:, j] * 1e3, label=lbl)[0])
+    labels.append(lbl)
 plt.xscale('log')
-plt.ylabel('C / ($10^{-3}$ mol / L)')
-plt.xlabel('t / s')
-plt.legend(frameon=False)
-plt.savefig('combination.pdf')
+plt.ylabel('C$_i$  (mM)')
+plt.xlabel('t  (s)')
+plt.legend(lines[::-1], labels[::-1], frameon=False)
+plt.savefig('fig_combination.pdf')
+
+# Other energies for the SI
+fig, ax = plt.subplots(figsize=(6,2.5), ncols=2)
+fig.subplots_adjust(bottom=.17, left=.1, right=.99, top=.98)
+
+# 24 keV
+ax[0].plot(t, av_concs[2, :, 0] * 1e3, label='OH$^\\bullet$')
+ax[0].plot(t, av_concs[2, :, 1] * 1e3, label='H$^\\bullet$')
+ax[0].plot(t, av_concs[2, :, 2] * 1e3, label='H$_\mathrm{2}$')
+ax[0].plot(t, av_concs[2, :, 3] * 1e3, label='H$_\mathrm{2}$O$_\mathrm{2}$')
+handles, labels = ax[0].get_legend_handles_labels()
+ax[0].legend(handles[::-1], labels[::-1], frameon=False)
+ax[0].set_xscale('log')
+ax[0].set_ylabel('C$_i$  (mM)')
+ax[0].set_xlabel('t  (s)')
+
+# 800 eV
+ax[1].plot(t, av_concs[0, :, 0] * 1e3, label='OH$^\\bullet$')
+ax[1].plot(t, av_concs[0, :, 1] * 1e3, label='H$^\\bullet$')
+ax[1].plot(t, av_concs[0, :, 2] * 1e3 * .1, label='H$_\mathrm{2}$ (x0.1)')
+ax[1].plot(t, av_concs[0, :, 3] * 1e3 * .1, label='H$_\mathrm{2}$O$_\mathrm{2}$ (x0.1)')
+handles, labels = ax[1].get_legend_handles_labels()
+ax[1].legend(handles[::-1], labels[::-1], frameon=False)
+ax[1].set_xscale('log')
+ax[1].set_xlabel('t  (s)')
+ax[1].text(0, 0, 'E=')
+
+fig.text(.005, .99, 'a)', va='top')
+fig.text(.525, .99, 'b)', va='top')
+
+plt.savefig('fig_combination_SI.pdf')
